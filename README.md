b0b2b06b0172febe259c2832c3dcaaa5c96d0473
juseok.lee <juseok.lee@monitorapp.com>
2023-06-05 14:17:07 +0900

    feat: new init

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# vulnalert
+# VulnAlert
 최신 취약점 알림 툴
+## Branch Rule
+master, develop, feature로 진행.  
+feature Branch의 경우 로컬 저장.(Remote x)  
+참고 사이트 : <https://gmlwjd9405.github.io/2018/05/11/types-of-git-branch.html>
+## Commit Rule
+제목만 규칙 지정. 나머지는 자유롭게..  
+`<type>: <subject>`   
+Type 규칙 :  
++ feat : 새로운 기능 추가, 기존의 기능을 요구 사항에 맞추어 수정
++ fix : 기능에 대한 버그 수정
++ chore : 패키지 매니저 수정, 그 외 기타 수정 ex) .gitignore
++ docs : 문서(주석) 수정
++ style : 코드 스타일, 포맷팅에 대한 수정
++ refactor : 기능의 변화가 아닌 코드 리팩터링 ex) 변수 이름 변경
++ test : 테스트 코드 추가/수정
++ release : 버전 릴리즈  
+
+Subject는 보기 쉬운 커밋 내용 요약 제목.  
+참고 사이트 : <https://velog.io/@jiheon/Git-Commit-message-%EA%B7%9C%EC%B9%99>
 ## venv 사용
 가상환경 들어갈 때 : `venv\Scripts\activate.bat`  
 나올 때 : `deactivate`  
--- main.py
+++ main.py
@@ -1,81 +1,23 @@
-import nvdlib
-from dotenv import load_dotenv
+import discord
+from discord.ext import commands
 import os
-import requests
-import json
-import datetime
-import sqlalchemy
-from sqlalchemy import create_engine
-import pandas as pd
-import numpy as np
 
+bot_token = os.environ['BOT_TOKEN']
+prefix = '!'
 
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-#main class
-class getVuln():
-    def __init__(self, user, passwd, address, dbname, tablename):
-        load_dotenv()
-        self.nvdapikey = os.environ.get("NVDAPI")
-        self.datadir = "./data/" 
-        self.nvd_df, self.cpe_df = self.sql_init(user, passwd, address, dbname, tablename)
-    
-    def sql_init(self, user, passwd, address, dbname):
-        try: 
-            nvd_df = pd.read_sql_table('NVD', f'mysql+pymysql://{user}:{passwd}@{address}/{dbname}')
-            cpe_df = pd.read_sql_table('CPE', f'mysql+pymysql://{user}:{passwd}@{address}/{dbname}')
-            return nvd_df, cpe_df
-        except Exception as e:
-            print(e)
-            
-    def get_nvd(self):
-        now_kst = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
-        # get new cve
-        new_cve = self.new_cve_by_nvd(now_kst - datetime.timedelta(days=1))
-        #get update cve
-        update_cve = self.update_cve_by_nvd(now_kst - datetime.timedelta(days=1))
-        self.update_db(new_cve, update_cve)
-  
-    
-    def new_cve_by_nvd(self, time):
-        startdate = time.strftime('%Y-%m-%d 00:00')
-        enddate = time.strftime('%Y-%m-%d 23:59')
-        res = nvdlib.searchCVE(pubStartDate=startdate, pubEndDate=enddate, key=self.nvdapikey)
-        result = {}
-        for i in res:
-            tmp = {}
-            tmp['id'] = i.id
-            tmp['assigner'] = i.sourceIdentifier
-            tmp['description'] = [x.value if x.lang == 'en' else '' for x in i.descriptions][0]
-            tmp['cwe'] = [x.value if x.lang == 'en' else '' for x in i.cwe] if 'cwe' in i else [] #list
-            tmp['reference'] = [x.url for x in i.references] #list
-            tmp['published'] = i.published
-            tmp['lastModified'] = i.lastModified
-            tmp['cpe'] = i.cpe if 'cpe' in i else '' #list
-            tmp['cvssv3'] = i.v30score if 'v30score' in i else 0.0 #float
-            tmp['cvssv2'] = i.v2score if 'v2score' in i else 0.0 #float
-            result[i.id] = tmp
-        return result
-            
-    def update_cve_by_nvd(self, time):
-       startdate = str(time.strftime('%Y-%m-%dT00:00:00'))+'%2b09:00'
-       enddate = str(time.strftime('%Y-%m-%dT23:59:59'))+'%2b09:00'
-       url = 'https://services.nvd.nist.gov/rest/json/cvehistory/2.0'
-       param = {'changeStartDate':startdate, 'changeEndDate':enddate}
-       stringParams = '&'.join([k if v is None else f"{k}={v}" for k, v in param.items()])
-       header = {'apikey':self.nvdapikey}
-       res = requests.get(url=url, headers=header, params=stringParams).json()
-       result = {}
-       for i in res['cveChanges']:
-           result[i['change']['cveId']] = i['change']['details']
-       return result
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    await bot.change_presence(status=discord.Status.online)
 
-    def update_db(self, new_nvd, update_nvd):
-        
-        
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-def main():
-    v = getVuln()
-    v.get_nvd()
-    
-if __name__ == "__main__":
-    main()
